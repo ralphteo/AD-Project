0f5d3c08613ba67a6215a9c5a5b89e52facbcf58
pingpong <e1582280@u.nus.edu>
2026-01-29 16:28:51 +0800

    update

--- ADWebApplication/Controllers/BinCollectionController.cs
+++ ADWebApplication/Controllers/BinCollectionController.cs
@@ -1,85 +1,85 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using ADWebApplication.Models;
-using Microsoft.AspNetCore.Mvc.Rendering;
+// using System.Diagnostics;
+// using Microsoft.AspNetCore.Mvc;
+// using ADWebApplication.Models;
+// using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace ADWebApplication.Controllers;
+// namespace ADWebApplication.Controllers;
 
-// Mark Bins as collected, submit timestamp and update bin level
-public class BinCollectionController : Controller
-{
+// // Mark Bins as collected, submit timestamp and update bin level
+// public class BinCollectionController : Controller
+// {
 
-    [HttpGet]
-    public IActionResult MarkCollected(int? routePlanId)
-    {
-        var model = new BinCollectionViewModel
-        {
-            RoutePlans = DummyRouteRepository.GetRoutePlans()
-        };
+//     [HttpGet]
+//     public IActionResult MarkCollected(int? routePlanId)
+//     {
+//         var model = new BinCollectionViewModel
+//         {
+//             RoutePlans = DummyRouteRepository.GetRoutePlans()
+//         };
 
-        if (routePlanId.HasValue)
-        {
-            model.SelectedRoutePlanId = routePlanId;
-            model.RouteStops = DummyRouteRepository.GetStopsForRoute(routePlanId.Value);
-        }
+//         if (routePlanId.HasValue)
+//         {
+//             model.SelectedRoutePlanId = routePlanId;
+//             model.RouteStops = DummyRouteRepository.GetStopsForRoute(routePlanId.Value);
+//         }
 
-        return View(model);
-    }
+//         return View(model);
+//     }
 
-    [HttpGet]
-    public IActionResult GetRouteStops(int routePlanId)
-    {
-        // To replace with actual database logic
-        var stops = DummyRouteRepository.GetStopsForRoute(routePlanId);
+//     [HttpGet]
+//     public IActionResult GetRouteStops(int routePlanId)
+//     {
+//         // To replace with actual database logic
+//         var stops = DummyRouteRepository.GetStopsForRoute(routePlanId);
 
-        // Return JSON
-        return Json(stops.Select(s => new
-        {
-            StopId = s.StopId,
-            StopSequence = s.StopSequence,
-            LocationName = s.CollectionBin?.LocationName
-        }));
-    }
+//         // Return JSON
+//         return Json(stops.Select(s => new
+//         {
+//             StopId = s.StopId,
+//             StopSequence = s.StopSequence,
+//             LocationName = s.CollectionBin?.LocationName
+//         }));
+//     }
 
-   [HttpPost]
-    public IActionResult MarkCollected(BinCollectionViewModel model)
-    {
-        // Get route plans on Post
-        model.RoutePlans = DummyRouteRepository.GetRoutePlans();
+//    [HttpPost]
+//     public IActionResult MarkCollected(BinCollectionViewModel model)
+//     {
+//         // Get route plans on Post
+//         model.RoutePlans = DummyRouteRepository.GetRoutePlans();
 
-         // Validate Route Stop selection
-        if (!model.SelectedRouteStopId.HasValue)
-        {
-            ModelState.AddModelError(nameof(model.SelectedRouteStopId), "Please select a Route Stop.");
-        }
+//          // Validate Route Stop selection
+//         if (!model.SelectedRouteStopId.HasValue)
+//         {
+//             ModelState.AddModelError(nameof(model.SelectedRouteStopId), "Please select a Route Stop.");
+//         }
 
-        // Convert local datetime to DateTimeOffset using client timezone offset
-        if (model.CollectionDetails != null)
-        {
-            var local = model.CollectionDetails.CollectionDateTimeLocal;
-            var offset = TimeSpan.FromMinutes(-model.TimezoneOffset);
-            model.CollectionDetails.CollectionDateTimeOffset = new DateTimeOffset(local, offset);
-        }
+//         // Convert local datetime to DateTimeOffset using client timezone offset
+//         if (model.CollectionDetails != null)
+//         {
+//             var local = model.CollectionDetails.CollectionDateTimeLocal;
+//             var offset = TimeSpan.FromMinutes(-model.TimezoneOffset);
+//             model.CollectionDetails.CollectionDateTimeOffset = new DateTimeOffset(local, offset);
+//         }
 
-        // Validate
-        if (!ModelState.IsValid)
-        {
-            if (model.SelectedRoutePlanId.HasValue)
-            {
-                model.RouteStops =
-                    DummyRouteRepository.GetStopsForRoute(model.SelectedRoutePlanId.Value);
-            }
-            return View(model);
-        }
+//         // Validate
+//         if (!ModelState.IsValid)
+//         {
+//             if (model.SelectedRoutePlanId.HasValue)
+//             {
+//                 model.RouteStops =
+//                     DummyRouteRepository.GetStopsForRoute(model.SelectedRoutePlanId.Value);
+//             }
+//             return View(model);
+//         }
 
-        // Save to DB Method
-        // ...
+//         // Save to DB Method
+//         // ...
 
-        return RedirectToAction(nameof(Success));
-    }
+//         return RedirectToAction(nameof(Success));
+//     }
 
-    public IActionResult Success()
-    {
-        return View();
-    }
-}
+//     public IActionResult Success()
+//     {
+//         return View();
+//     }
+// }
