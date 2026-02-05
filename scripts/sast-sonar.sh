344a6f49031a9e1904917f344af895dc61e56015
AT-WhemblyWhermer <e1674574@u.nus.edu>
2026-02-05 23:51:38 +0800

    debug sast artifact

--- scripts/sast-sonar.sh
+++ scripts/sast-sonar.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
+# Start Sonar analysis
 dotnet sonarscanner begin \
   /k:"GDipSA-Team-5_AD-Project" \
   /o:"gdipsa-team-5" \
   /d:sonar.host.url="https://sonarcloud.io" \
+  /d:sonar.login="$SONAR_TOKEN" \
+  /d:sonar.projectBaseDir="." \
   /d:sonar.cs.cobertura.reportsPaths="**/coverage.cobertura.xml" \
   /d:sonar.coverage.exclusions="**/Program.cs"
 
+# Build (required)
 dotnet build ./ADWebApplication/ADWebApplication.csproj
 
-dotnet sonarscanner end
+# Run tests WITH coverage (this is what you asked about)
+dotnet test ./ADWebApplication/ADWebApplication.csproj \
+  --collect:"XPlat Code Coverage" \
+  --results-directory ./TestResults
+
+# End Sonar analysis (uploads results)
+dotnet sonarscanner end \
+  /d:sonar.login="$SONAR_TOKEN"
--- scripts/test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-dotnet test ./ADWebApplication.Tests/ADWebApplication.Tests.csproj \
-  --collect:"XPlat Code Coverage"
