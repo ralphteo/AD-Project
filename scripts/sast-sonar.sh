a88c51667852b8d3fedd522e49cb6a2655ba3123
AT-WhemblyWhermer <e1674574@u.nus.edu>
2026-02-06 00:57:13 +0800

    debug sast

--- scripts/sast-sonar.sh
+++ scripts/sast-sonar.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# Start SonarCloud scan
-dotnet sonarscanner begin \
-  /k:"GDipSA-Team-5_AD-Project" \
-  /o:"${SONAR_ORG}" \
-  /d:sonar.host.url="https://sonarcloud.io" \
-  /d:sonar.login="${SONAR_TOKEN}" \
-  /d:sonar.projectBaseDir="." \
-  /d:sonar.cs.cobertura.reportsPaths="./TestResults/CoverageReport/Cobertura.xml" \
-  /d:sonar.coverage.exclusions="**/Program.cs"
-
 # Build main project
 dotnet build ./ADWebApplication/ADWebApplication.csproj
 
@@ -19,12 +9,22 @@ dotnet test ./ADWebApplication.Tests/ADWebApplication.Tests.csproj \
   --collect:"XPlat Code Coverage" \
   --results-directory ./TestResults
 
-# Convert coverage to a (cobertura) format for SonarCloud
-dotnet tool install --global dotnet-reportgenerator-globaltool
+# Convert coverage to Cobertura format for SonarCloud
+dotnet tool install --global dotnet-reportgenerator-globaltool --version 5.5.1
 reportgenerator \
   -reports:./TestResults/*/coverage.cobertura.xml \
   -targetdir:./TestResults/CoverageReport \
   -reporttypes:Cobertura
+
+# Start SonarCloud scan
+dotnet sonarscanner begin \
+  /k:"GDipSA-Team-5_AD-Project" \
+  /o:"${SONAR_ORG}" \
+  /d:sonar.host.url="https://sonarcloud.io" \
+  /d:sonar.login="${SONAR_TOKEN}" \
+  /d:sonar.sources="./ADWebApplication" \
+  /d:sonar.cs.cobertura.reportsPaths="./TestResults/CoverageReport/Cobertura.xml" \
+  /d:sonar.coverage.exclusions="**/Program.cs"
 
 # End SonarCloud scan
 dotnet sonarscanner end \
